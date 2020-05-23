df4dd906ea47c14c4d47780ad4d8e7d10d41086a
nabeen <watanabe_kenichiro@hasigo.co.jp>
2020-05-23 16:49:31 +0900

    feat: add command skeleton

--- logist/cli.py
+++ logist/cli.py
@@ -1,16 +1,38 @@
 """Console script for logist."""
 import sys
 import click
+import setting
+import backlog
+import todoist
 
 
-@click.command()
-def main(args=None):
-    """Console script for logist."""
-    click.echo("Replace this message by putting your code into "
-               "logist.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+@click.group()
+def cmd():
+    pass
+
+
+@cmd.command(help='show issues from Backlog')
+def show():
+    click.echo('Start show...')
+    backlog.main()
+
+
+@cmd.command(help='export issues to Todoist')
+@click.option('-id', '--issues-id', 'id', help='Input issues-id separated with comma', required=True)
+def export(id):
+    click.echo('Start export...')
+    todoist.main()
+
+
+@cmd.command(help='configure your setting')
+def configure():
+    click.echo('Start configure...')
+    setting.main()
+
+
+def main():
+    cmd()
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    main()  # pragma: no cover
