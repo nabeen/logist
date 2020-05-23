505becbf955675eca83236a76d38a95754558233
nabeen <watanabe_kenichiro@hasigo.co.jp>
2020-05-23 23:01:36 +0900

    feat: mod config reader

--- logist/viewer.py
+++ logist/viewer.py
@@ -2,22 +2,29 @@ from pybacklogpy.BacklogConfigure import BacklogComConfigure
 from pybacklogpy.Issue import Issue
 import json
 import configparser
+import click
+import setting
 
 
 def main():
-    print('backlog')
-    config_file = configparser.ConfigParser()
-    config_file.read('secrets')
+    try:
+        host = setting.read_config('backlog', 'Host')
+        api_key = setting.read_config('backlog', 'ApiKey')
+        assignee_id = setting.read_config('backlog', 'AssigneeId')
+    except Exception as e:
+        click.secho('set config with command configure before export', fg='red')
+        return
 
-    issue_api = Issue()
+    space_key = host.replace(".backlog.com", "")
+    config = BacklogComConfigure(space_key=space_key, api_key=api_key)
+    issue_api = Issue(config)
     # TODO: more filter
-    response = issue_api.get_issue_list(
-        assignee_id=[config_file['backlog']['AssigneeId']])
+    response = issue_api.get_issue_list(assignee_id=[assignee_id])
 
     if not response.ok:
         raise ValueError('failed get issues')
 
     data = json.loads(response.text)
     for v in data:
-        print(v['summary'], 'https://'+config_file['backlog']
-              ['Host']+'/view/'+v['issueKey'])
+        print(v['summary'], 'https://'+host+'/view/'+v['issueKey'])
+    click.secho('SUCCESS!!', fg='green')
